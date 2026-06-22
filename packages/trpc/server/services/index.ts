d641e596ce419792ab4a0d63638fae157efdc103
sahilthakurr24 <tsahil6231@gmail.com>
2026-06-22 22:31:56 +0530

    Add MembershipService to the TRPC server services for enhanced functionality.

--- packages/trpc/server/services/index.ts
+++ packages/trpc/server/services/index.ts
@@ -1,6 +1,7 @@
 import UserService from "@repo/services/user";
 import OrganizationService from "@repo/services/organization";
+import MembershipService from "@repo/services/membership";
 
 export const userService = new UserService();
 export const organizationService = new OrganizationService();
-
+export const membershipService = new MembershipService();
