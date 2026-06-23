54370f6b28e93ca39e47f5042c7188a211f36f12
sahilthakurr24 <tsahil6231@gmail.com>
2026-06-23 14:28:08 +0530

    Add env:link script to package.json and update pnpm-lock.yaml with new dependencies for better-auth and related packages.

---
+++ packages/auth/env.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+    BETTER_AUTH_SECRET : z.string().describe('Better auth secret key')
+});
+
+function createEnv(env: NodeJS.ProcessEnv) {
+  const safeParseResult = envSchema.safeParse(env);
+  if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+  return safeParseResult.data;
+}
+
+export const env = createEnv(process.env);
